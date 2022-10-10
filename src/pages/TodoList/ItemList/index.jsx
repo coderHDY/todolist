77a9fa9b95ae5e8011db68ac44e46e17d1767601
coderHDY <huangdeyu2020@163.com>
2022-10-10 22:12:26 +0800

    剪切板

--- src/pages/TodoList/ItemList/index.jsx
+++ src/pages/TodoList/ItemList/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import styles from "./index.module.css";
 import {
   List,
@@ -6,6 +6,8 @@ import {
   IconButton,
   ListItemText,
   Checkbox,
+  Snackbar,
+  Alert,
 } from '@mui/material';
 
 import {
@@ -15,38 +17,55 @@ import dayjs from "dayjs";
 
 export default function ItemList({ list, del, toggleDone }) {
   const ul = useRef();
+  const [showTip, setShowTip] = useState(false);
   useEffect(() => {
-    const observer = new IntersectionObserver(([e]) => {
-      if (e.isIntersecting) {
-        e.target.classList.add(styles.active);
-        observer.unobserve(e.target);
-      }
-    })
-    Array.prototype.forEach.call(ul.current.children, (item, idx) => setTimeout(() => observer.observe(item), idx * 40));
+    Array.prototype.forEach.call(ul.current.children, (item, idx) => setTimeout(() => item.classList.add(styles.active), idx * 40));
   }, []);
+  const copy = (val) => () => {
+    if (!navigator.clipboard) return;
+    const timeout = setTimeout(() => {
+      navigator.clipboard.writeText(val);
+      setShowTip(true);
+      setTimeout(() => setShowTip(false), 1000);
+    }, 700);
+    const touchEnd = () => {
+      clearTimeout(timeout);
+      window.removeEventListener('touchend', touchEnd);
+    }
+    window.addEventListener('touchend', touchEnd);
+  }
   return (
-    <List dense ref={ul} className={styles.list}>
-      {
-        list.map(item => (
-          <ListItem
-            key={item.id}
-            divider
-            secondaryAction={
-              <IconButton edge="end" aria-label="delete" onClick={() => del(item.id)}>
-                <DeleteIcon color='error' />
-              </IconButton>
-            }
-            className={`${styles.item} ${item.done ? styles.done : ""}`}
-          >
-            <Checkbox className={item.done ? styles.done : ""} checked={item.done} onChange={() => toggleDone(item.id)} />
-            <ListItemText
-              primary={item.val}
-              secondary={item.deadline && `时间: ${dayjs(new Date(item.deadline)).format("YYYY-MM-DD HH:mm")}`}
-              className={styles.text}
-            />
-          </ListItem>
-        )
-        )}
-    </List>
+    <>
+      <List dense ref={ul} className={styles.list}>
+        {
+          list.map(item => (
+            <ListItem
+              key={item.id}
+              onTouchStart={copy(item.val)}
+              divider
+              secondaryAction={
+                <IconButton edge="end" aria-label="delete" onClick={() => del(item.id)}>
+                  <DeleteIcon color='error' />
+                </IconButton>
+              }
+              className={`${styles.item} ${item.done ? styles.done : ""}`}
+            >
+              <Checkbox className={item.done ? styles.done : ""} checked={!!item.done} onChange={() => toggleDone(item.id)} />
+              <ListItemText
+                primary={item.val}
+                secondary={item.deadline && `时间: ${dayjs(new Date(item.deadline)).format("YYYY-MM-DD HH:mm")}`}
+                className={styles.text}
+              />
+            </ListItem>
+          )
+          )}
+      </List>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={showTip}
+      >
+        <Alert severity="success">复制成功</Alert>
+      </Snackbar>
+    </>
   )
 }
