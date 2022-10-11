aa92f924c8471776f9020b6aca354382e60bc807
coderHDY <huangdeyu2020@163.com>
2022-10-11 22:58:38 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useRoutes } from 'react-router-dom';
 import routerMap from './routes';
 import TabBar from './components/TabBar';
+import Loading from "./components/Loading";
+
 function App() {
   const routes = useRoutes(routerMap);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 1000);
+  }, []);
   return (
     <>
-      { routes }
+      {loading ? <Loading /> : (routes)}
       <TabBar />
     </>
   );
